6c39fff17e7ed56df19c92a640266af513b68da9
Aeglasin <aeglasin@gmx.net>
2016-05-16 15:49:40 +0200

    added KinectStub

---
+++ SmartMirror/SmartMirror/Common/Kinect/KinectInteraction.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Kinect;
+namespace SmartMirror.Common.Kinect
+{
+    class KinectInteraction
+    {
+        #region "Variables and constants"
+        /// <summary>
+        /// Active Kinect sensor
+        /// </summary>
+        private KinectSensor kinectSensor = null;
+
+        /// <summary>
+        /// Coordinate mapper to map one type of point to another
+        /// </summary>
+        private CoordinateMapper coordinateMapper = null;
+
+        /// <summary>
+        /// Reader for body frames
+        /// </summary>
+        private BodyFrameReader bodyFrameReader = null;
+
+        /// <summary>
+        /// Array for the bodies
+        /// </summary>
+        private Body[] bodies = null;
+
+                /// <summary>
+        /// Current status text to display
+        /// </summary>
+        private string statusText = null;
+        /// <summary>
+        /// Constant for clamping Z values of camera space points from being negative
+        /// </summary>
+        private const float InferredZPositionClamp = 0.1f;
+        public delegate void HandsDeteced(List<Hands> allHands);
+        public event HandsDeteced newHandsFrameDetected;
+        #endregion
+
+
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public KinectInteraction()
+        {
+            // one sensor is currently supported
+            this.kinectSensor = KinectSensor.GetDefault();
+
+            // get the coordinate mapper
+            this.coordinateMapper = this.kinectSensor.CoordinateMapper;
+
+            // get the depth (display) extents
+            FrameDescription frameDescription = this.kinectSensor.DepthFrameSource.FrameDescription;
+
+            
+
+            // open the reader for the body frames
+            this.bodyFrameReader = this.kinectSensor.BodyFrameSource.OpenReader();
+
+            
+
+            // set IsAvailableChanged event notifier
+            this.kinectSensor.IsAvailableChanged += this.Sensor_IsAvailableChanged;
+
+            // open the sensor
+            this.kinectSensor.Open();
+
+            // set the status text
+            this.StatusText = this.kinectSensor.IsAvailable ? Properties.Resources.RunningStatusText
+                                                            : Properties.Resources.NoSensorStatusText;
+        }
+
+        /// <summary>
+        /// INotifyPropertyChangedPropertyChanged event to allow window controls to bind to changeable data
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        /// <summary>
+        /// Gets or sets the current status text to display
+        /// </summary>
+        public string StatusText
+        {
+            get
+            {
+                return this.statusText;
+            }
+
+            set
+            {
+                if (this.statusText != value)
+                {
+                    this.statusText = value;
+
+                    // notify any bound elements that the text has changed
+                    if (this.PropertyChanged != null)
+                    {
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("StatusText"));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Execute start up tasks
+        /// </summary>
+        public void finializeInit()
+        {
+            if (this.bodyFrameReader != null)
+            {
+                this.bodyFrameReader.FrameArrived += this.Reader_FrameArrived;
+            }
+        }
+
+        /// <summary>
+        /// Execute shutdown tasks
+        /// </summary>
+        public void closeKinect(object sender, CancelEventArgs e)
+        {
+            if (this.bodyFrameReader != null)
+            {
+                // BodyFrameReader is IDisposable
+                this.bodyFrameReader.Dispose();
+                this.bodyFrameReader = null;
+            }
+
+            if (this.kinectSensor != null)
+            {
+                this.kinectSensor.Close();
+                this.kinectSensor = null;
+            }
+        }
+
+        /// <summary>
+        /// Handles the body frame data arriving from the sensor
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Reader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            bool dataReceived = false;
+
+            using (BodyFrame bodyFrame = e.FrameReference.AcquireFrame())
+            {
+                if (bodyFrame != null)
+                {
+                    if (this.bodies == null)
+                    {
+                        this.bodies = new Body[bodyFrame.BodyCount];
+                    }
+
+                    // The first time GetAndRefreshBodyData is called, Kinect will allocate each Body in the array.
+                    // As long as those body objects are not disposed and not set to null in the array,
+                    // those body objects will be re-used.
+                    bodyFrame.GetAndRefreshBodyData(this.bodies);
+                    dataReceived = true;
+                }
+            }
+
+            if (dataReceived)
+            {
+                List<Hands> allHands = new List<Hands>();
+                foreach (Body body in this.bodies)
+                {
+                    Hands hands=null;
+                    if (body.IsTracked)
+                    {
+                        IReadOnlyDictionary<JointType, Joint> joints = body.Joints;
+                        CameraSpacePoint positionLeft = joints[JointType.HandLeft].Position;
+                        CameraSpacePoint positionRight = joints[JointType.HandRight].Position;                                              
+                        if (positionRight.Z < 0)
+                        {
+                            positionRight.Z = InferredZPositionClamp;
+                        }
+                        if (positionLeft.Z < 0)
+                        {
+                            positionLeft.Z = InferredZPositionClamp;
+                        }
+                        DepthSpacePoint depthSpacePointLeft = this.coordinateMapper.MapCameraPointToDepthSpace(positionLeft);
+                        DepthSpacePoint depthSpacePointRight = this.coordinateMapper.MapCameraPointToDepthSpace(positionRight);
+                        hands = new Hands(new Point(depthSpacePointLeft.X, depthSpacePointLeft.Y),
+                            new Point(depthSpacePointRight.X, depthSpacePointRight.Y));
+                    }
+
+                    if (hands != null)
+                    {
+                        allHands.Add(hands);
+                    }
+                }
+                newHandsFrameDetected(allHands);
+            }
+        }
+
+        /// <summary>
+        /// Handles the event which the sensor becomes unavailable (E.g. paused, closed, unplugged).
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Sensor_IsAvailableChanged(object sender, IsAvailableChangedEventArgs e)
+        {
+            // on failure, set the status text
+            this.StatusText = this.kinectSensor.IsAvailable ? Properties.Resources.RunningStatusText
+                                                            : Properties.Resources.SensorNotAvailableStatusText;
+        }
+    }
+
+    class Hands
+    {
+        Point HandLeft, HandRight;
+        public Hands(Point HandLeft, Point HandRight)
+        {
+            this.HandLeft = HandLeft;
+            this.HandRight = HandRight;
+        }
+    }
+
+}
+
+
+
+
